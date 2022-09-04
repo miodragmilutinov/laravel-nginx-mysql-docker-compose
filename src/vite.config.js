36476a9a848f244d4cfade9a7fdec73bade91990
Miodrag Milutinov <miodrag.milutinov@marbleit.rs>
2022-09-04 23:19:06 +0200

    Move vite to 3000

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: 'localhost',
+        port: 3000,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
