4329e85b211aa1aaddfaf0d952b385b8c3c39747
Miodrag Milutinov <miodrag.milutinov@marbleit.rs>
2022-09-06 12:37:20 +0200

    Configure vite ports and hmr

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,14 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    server: {
-        host: 'localhost',
-        port: 3000,
+    server:{
+        hmr: {
+          host: 'localhost'
+        },
+        host: 'localhost'
     },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
+            input: ['resources/js/app.js', 'resources/css/app.css'],
+            refresh: true
         }),
         vue({
             template: {
